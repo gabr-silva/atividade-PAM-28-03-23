0d0a5daeb520e96d5b427b4f8983968d7a277a4f
gabr-silva <gabrielvieira.s0801@gmail.com>
2023-04-18 19:19:09 -0300

    oi

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-import "expo-router/entry";
+import React, {useState, useEffect} from 'react';
+import {View, Text, TextInput, TouchableOpacity, Pressable} from 'react-native';
+import {Link} from 'expo-router';
+ 
+export default function Cadastro () {
+    return (
+        <View>
+            <Text>Cadastro de Endereço</Text>
+            <TextInput value={'00000-000'} />
+            <TouchableOpacity>
+                <Text>Buscar Dados</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
